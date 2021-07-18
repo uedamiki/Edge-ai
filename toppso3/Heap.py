4546f11359c12f528235402355085966b002627c
uedamiki <91820992+uedamiki@users.noreply.github.com>
2021-07-18 09:41:40 +0000

    Set version

--- toppso3/Heap.py
+++ toppso3/Heap.py
@@ -1,4 +1,7 @@
 import numpy as np
 
 class Heap():
-    """Heap is a class of bin
+    """Heap is a class of binary min. heap.
+       Attributes:
+           data    -- a list of data
+           
