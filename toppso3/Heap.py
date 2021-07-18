722815efd840bd0cc36e3a7c411ec93740710d5a
uedamiki <91820992+uedamiki@users.noreply.github.com>
2021-07-18 12:36:09 +0000

    Updated .gitignore files

--- toppso3/Heap.py
+++ toppso3/Heap.py
@@ -4,4 +4,5 @@ class Heap():
     """Heap is a class of binary min. heap.
        Attributes:
            data    -- a list of data
-           
+           size    -- the number of data in the list
+           nlevels -- level of the 
