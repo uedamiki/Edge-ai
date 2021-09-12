45273a4002ddcdd110b6bf8c8f19bbb7dfa9d623
uedamiki <91820992+uedamiki@users.noreply.github.com>
2021-09-12 10:44:56 +0000

    1.2.20

--- toppso3/SE3RRT.py
+++ toppso3/SE3RRT.py
@@ -253,4 +253,5 @@ class RRTPlanner():
             ## interpolate a trajectory
             #trajectory = lie.InterpolateSO3ZeroOmega(rotationMatrixFromQuat(q_beg),rotationMatrixFromQuat(q_end),self.INTERPOLATIONDURATION)
             trajectory = lie.InterpolateSO3(rotationMatrixFromQuat(q_beg),rotationMatrixFromQuat(q_end),qs_beg,qs_end,self.INTERPOLATIONDURATION)
-            trajectorytranstring = Utils.TrajStri
+            trajectorytranstring = Utils.TrajString3rdDegree(qt_beg, qt_end, qts_beg, qts_end, self.INTERPOLATIONDURATION)
+            ##
