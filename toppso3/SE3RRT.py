261e7e4f9ab4714933bc1a55aed2889d144bedc1
uedamiki <91820992+uedamiki@users.noreply.github.com>
2021-09-18 10:53:27 +0000

    Run scan-local.sh on mysql:...

--- toppso3/SE3RRT.py
+++ toppso3/SE3RRT.py
@@ -277,4 +277,4 @@ class RRTPlanner():
             qt_end = v_near.config.qt
             qts_end = v_near.config.qts
             ## check if c_rand is too far from vnear
-            ## if the new ramdonly-chose node is clos
+            ## if the new ramdonly-chose node is close, it's safer . Or in another words, the interpolated path will have m
