cb96aa7fc47bb599f7feaf811c50b5e01488d527
uedamiki <91820992+uedamiki@users.noreply.github.com>
2021-09-22 09:09:16 +0000

    client: updated against Yield

--- toppso3/SE3RRT.py
+++ toppso3/SE3RRT.py
@@ -284,4 +284,8 @@ class RRTPlanner():
                 STATUS = REACHED
             else:
                 q_beg = q_end + self.STEPSIZE*(c_rand.q - q_end)/np.sqrt(delta)
-                q_beg /= np.lina
+                q_beg /= np.linalg.norm(q_beg)
+                STATUS = ADVANCED
+            qs_beg = c_rand.qs
+
+          
