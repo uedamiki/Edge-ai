8ebf2226f1f97a6c76d6fba9db75cb1a7076a530
uedamiki <91820992+uedamiki@users.noreply.github.com>
2021-10-11 21:40:14 +0000

    Increased coverage

--- toppso3/SE3RRT.py
+++ toppso3/SE3RRT.py
@@ -374,4 +374,5 @@ class RRTPlanner():
 
             ## interpolate a trajectory
             #trajectory = lie.InterpolateSO3ZeroOmega(rotationMatrixFromQuat(q_beg),rotationMatrixFromQuat(q_end),self.INTERPOLATIONDURATION)
-            trajectory = lie.InterpolateSO3(rotationMatrixFromQuat(q_beg),rotationMatrixFrom
+            trajectory = lie.InterpolateSO3(rotationMatrixFromQuat(q_beg),rotationMatrixFromQuat(q_end),qs_beg,qs_end,self.INTERPOLATIONDURATION)
+            trajectorytranstring = Utils.TrajString3rdDegr
