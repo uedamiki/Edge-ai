39e1987f6fcf9aa1f57601fb30d0c5bbb76e4c15
uedamiki <91820992+uedamiki@users.noreply.github.com>
2021-11-02 19:16:32 +0000

    ready to deploy

--- toppso3/SE3RRT.py
+++ toppso3/SE3RRT.py
@@ -502,4 +502,8 @@ class RRTPlanner():
         else:
             nn = custom_nn
         
-        if (nn == -1): #using all of the vertex
+        if (nn == -1): #using all of the vertexes in the tree
+            nn = nv
+        else:
+            nn = min(self.nn, nv)
+        nn
