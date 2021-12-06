8356dff444b09913c8d36b8499f5dade62e6740d
uedamiki <91820992+uedamiki@users.noreply.github.com>
2021-12-06 23:11:44 +0000

    Updated build demo

--- toppso3/SO3RRT.py
+++ toppso3/SO3RRT.py
@@ -140,4 +140,7 @@ class RRTPlanner():
 
     def __init__(self, vertex_start, vertex_goal, robot):
         """Initialize a planner. RRTPlanner always has two trees. For a unidirectional planner, 
-        the treeend will not be extended and always has only one vertex, vertex_goal.  
+        the treeend will not be extended and always has only one vertex, vertex_goal.        
+        """        
+        # np.random.seed(np.random.randint(0, 10))
+        ## 
