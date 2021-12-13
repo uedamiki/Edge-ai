c57333e396615abb6a5ada7983e3d16e5d6c6b00
uedamiki <91820992+uedamiki@users.noreply.github.com>
2021-12-13 17:42:25 +0000

    ASoC: sta32x: Fix array access overflow

--- toppso3/SO3RRT.py
+++ toppso3/SO3RRT.py
@@ -171,4 +171,7 @@ class RRTPlanner():
 
     def RandomConfig(self):
         """RandomConfig samples a random configuration uniformly from the quaternion unit sphere in four dimensions."""
-        q_rand 
+        q_rand = lie.RandomQuat()
+        vellowerlimit = -5 ##
+        velupperlimit = 5  ##
+        
