0629ad2d5e0f79c7466ea9027b233190af39769b
uedamiki <91820992+uedamiki@users.noreply.github.com>
2021-12-28 21:13:22 +0000

    note for readme

--- toppso3/SO3RRT.py
+++ toppso3/SO3RRT.py
@@ -239,4 +239,5 @@ class RRTPlanner():
             qs_end = v_near.config.qs
 
             ## check if c_rand is too far from vnear
-            ## if the new ramdonly-chose node is close, it's safer . Or in another words, the interpolated path 
+            ## if the new ramdonly-chose node is close, it's safer . Or in another words, the interpolated path will have more chances that it won't collide with the obstacles
+            delta = self.D
