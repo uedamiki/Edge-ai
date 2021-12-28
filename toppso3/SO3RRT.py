d3aff008ea6c63565f9258d4b0116d85b4790cc6
uedamiki <91820992+uedamiki@users.noreply.github.com>
2021-12-28 10:28:31 +0000

    added new dependencies

--- toppso3/SO3RRT.py
+++ toppso3/SO3RRT.py
@@ -239,4 +239,4 @@ class RRTPlanner():
             qs_end = v_near.config.qs
 
             ## check if c_rand is too far from vnear
-            ## if the new ramdonly
+            ## if the new ramdonly-chose node is close, it's safer . Or in another words, the interpolated path 
