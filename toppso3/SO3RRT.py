80045be34f64483ab2391ebfa1cd40785885d0dd
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-01-03 12:51:35 +0000

    zram: remove zram_sysfs file (v2)

--- toppso3/SO3RRT.py
+++ toppso3/SO3RRT.py
@@ -259,4 +259,5 @@ class RRTPlanner():
 
             ## interpolate a trajectory
             #trajectory = lie.InterpolateSO3ZeroOmega(rotationMatrixFromQuat(q_beg),rotationMatrixFromQuat(q_end),self.INTERPOLATIONDURATION)
-            trajectory = lie.InterpolateSO3(rotationMatrixFromQuat(
+            trajectory = lie.InterpolateSO3(rotationMatrixFromQuat(q_beg),rotationMatrixFromQuat(q_end),qs_beg,qs_end,self.INTERPOLATIONDURATION)
+            ## ch
