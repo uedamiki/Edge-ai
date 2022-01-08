3f0a488684e438caa18f273ca0b3156975558f28
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-01-08 16:46:06 +0000

    USB: serial: option: add support for Quectel UC20

--- toppso3/SO3RRT.py
+++ toppso3/SO3RRT.py
@@ -286,4 +286,6 @@ class RRTPlanner():
         
     def ConnectFW(self):
         v_test = self.treeend.verticeslist[-1]
-        nnindices = self.NearestNeighbor
+        nnindices = self.NearestNeighborIndices(v_test.config, FW)
+        for index in nnindices:
+            v_near = self.treesta
