b9809418210885ac98dbcabcea449e1cc50c567e
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-01-09 07:25:42 +0000

    fix connection

--- toppso3/SO3RRT.py
+++ toppso3/SO3RRT.py
@@ -288,4 +288,7 @@ class RRTPlanner():
         v_test = self.treeend.verticeslist[-1]
         nnindices = self.NearestNeighborIndices(v_test.config, FW)
         for index in nnindices:
-            v_near = self.treesta
+            v_near = self.treestart.verticeslist[index]
+            
+            q_beg = v_near.config.q
+            qs_beg = 
