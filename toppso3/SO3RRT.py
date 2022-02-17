dcedf37c1d55c496ebca791f5f76138a4eed57f0
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-02-17 06:34:43 +0000

    remove package

--- toppso3/SO3RRT.py
+++ toppso3/SO3RRT.py
@@ -468,4 +468,6 @@ class RRTPlanner():
         RotationMatrixList = []
         RotationMatrixList = self.treestart.GenRotationMatList()
         if (self.treeend.GenRotationMatList() != []):
-            RotationMatrixList.extend(s
+            RotationMatrixList.extend(self.treeend.GenRotationMatList())
+        RotationMatrixList.pop()
+        #pri
