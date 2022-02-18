51e7592cb84d7e16a94a74abac47b2a1f263dc8b
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-02-18 19:03:48 +0000

    netfilter: ipset: small potential read beyond the end of buffer

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -25,4 +25,7 @@ def QuatDistance(quat0, quat1):
     quatDistance = rotationweight*(1-abs(innerProduct))
     return quatDistance
 
-def SO3Distance(R0, R
+def SO3Distance(R0, R1): # bi-invariance
+    return linalg.norm(lie.logvect(dot(R0.T,R1)))
+
+def R3Distance(b0, 
