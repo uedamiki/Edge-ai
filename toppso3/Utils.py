94d11b9211a47864431fbe521ec570162e8c296c
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-03-01 07:28:46 +0000

    Reviewed by Dave.

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -82,4 +82,6 @@ def TransRotTrajFromSE3Traj(SE3traj):
     for c in SE3traj.chunkslist:
         transchunk = Trajectory.Chunk(c.duration, c.polynomialsvector[:3])
         transclist.append(transchunk)
-        rchunk = Trajectory.Chu
+        rchunk = Trajectory.Chunk(c.duration, c.polynomialsvector[3:])
+        rclist.append(rchunk)
+    transtraj = Traject
