84cc1e74596861d8406d8eb3fd96b766ba49fd9f
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-03-16 12:36:21 +0000

    Added spinner

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -163,4 +163,5 @@ def CheckCollisionSE3Traj( robot, transtraj, rtraj, R_beg,  checkcollisiontimest
             transformation[0:3,0:3] = lie.EvalRotation(R_beg, rtraj, s)
             transformation[0:3,3] = transtraj.Eval(s)
             robot.SetTransform(transformation)           
-            isincoll
+            isincollision = (env.CheckCollision(robot, CollisionReport()))
+            #print  "s 
