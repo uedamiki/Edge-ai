b5e2513383e3bfd0018517a3c8b086fcc68187d3
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-03-24 11:11:58 +0000

    Fix changelog date

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -221,4 +221,5 @@ def SE3Shortcut(robot, taumax, fmax, vmax, se3traj, Rlist, maxiter, expecteddura
                 print "\033[1;32mTrajectory's duration is already shorter than expected time --> stop shortcuting\033[0m"
                 break
         if (dur < discrtimestep):
-            print "[Utils::Sho
+            print "[Utils::Shortcut] trajectory duration is less than discrtimestep.\n"
+            break ## otherwise, this will 
