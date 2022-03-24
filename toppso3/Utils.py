228586d2b59b12dcb00e9a048e3f71a1379924e2
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-03-24 17:40:15 +0000

    initial version

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -228,4 +228,6 @@ def SE3Shortcut(robot, taumax, fmax, vmax, se3traj, Rlist, maxiter, expecteddura
         t0 = random.rand()* dur
         
         if meanduration == 0:
-          
+            meanduration = dur - t0
+            
+        T = random.rand()*min(meanduration,
