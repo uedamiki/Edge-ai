7bbffbec2c27ba0f3405b5b404e72a6eedb60aac
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-03-28 09:52:24 +0000

    Setting version to 1.1.1

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -253,4 +253,6 @@ def SE3Shortcut(robot, taumax, fmax, vmax, se3traj, Rlist, maxiter, expecteddura
         # print t0, t1, t1- t0       
         # interpolate from t0 to t1
         R_beg = lietraj.EvalRotation(t0)
-    
+        R_end = lietraj.EvalRotation(t1)
+        omega0 = lietraj.EvalOmega(t0)
+        omega1 = lietraj.
