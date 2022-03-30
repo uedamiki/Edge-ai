124d3426dcfc93ff767221c3bdfd08311b332778
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-03-30 20:47:13 +0000

    Create _posts

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -270,4 +270,5 @@ def SE3Shortcut(robot, taumax, fmax, vmax, se3traj, Rlist, maxiter, expecteddura
         
         isincollision = CheckCollisionSE3Traj(robot, shortcuttranstraj, shortcutrtraj, R_beg, discrtimestep)
         if (not isincollision):
-            a,b,c = ComputeSE3Constraints(shortcutse3traj,
+            a,b,c = ComputeSE3Constraints(shortcutse3traj, taumax, fmax, discrtimestep)
+            topp_inst = TOPP.QuadraticConstraints(shortcutse3tr
