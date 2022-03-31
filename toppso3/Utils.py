7093a3a06be20aafbfeb55607fb22536ceaa3173
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-03-31 22:14:40 +0000

    mm: vmscan: do not throttle based on pfmemalloc reserves if node has no ZONE_NORMAL

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -277,4 +277,5 @@ def SE3Shortcut(robot, taumax, fmax, vmax, se3traj, Rlist, maxiter, expecteddura
             if (ret == 1):
                 x.resduration
                 ## check whether the new one has shorter duration
-                if (
+                if (x.resduration + 0.1 < T): #skip if not shorter than 0.1 s
+            
