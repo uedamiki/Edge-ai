383c120a7b1892a62cc4e5dea498405b1520d287
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-04-04 07:25:40 +0000

    xhci: Fix race between ep halt and URB cancellation

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -285,4 +285,6 @@ def SE3Shortcut(robot, taumax, fmax, vmax, se3traj, Rlist, maxiter, expecteddura
                     TOPPed_shortcuttranstraj, TOPPed_shortcutrtraj = TransRotTrajFromSE3Traj(TOPPed_shortcutse3traj)
                     newlietraj = ReplaceTrajectorySegment(lietraj, TOPPed_shortcutrtraj , t0, t1)
                    
-                    newtranstraj = ReplaceTransTraj
+                    newtranstraj = ReplaceTransTrajectorySegment(transtraj, TOPPed_shortcuttranstraj, t0,t1)
+
+                    ###
