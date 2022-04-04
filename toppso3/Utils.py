e1cc1a385784b6d0ab6ecf14787bff27236a026a
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-04-04 17:07:11 +0000

    Update routes.php

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -288,4 +288,5 @@ def SE3Shortcut(robot, taumax, fmax, vmax, se3traj, Rlist, maxiter, expecteddura
                     newtranstraj = ReplaceTransTrajectorySegment(transtraj, TOPPed_shortcuttranstraj, t0,t1)
 
                     #####################################################
-                    newrtraj = Traje
+                    newrtraj = Trajectory.PiecewisePolynomialTrajectory.FromString(TrajStringFromTrajList(newlietraj.trajlist))
+ 
