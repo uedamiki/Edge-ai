631278ddc57b63a20dc7ff179eff20d547871bf1
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-04-07 19:37:59 +0000

    git test

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -302,4 +302,5 @@ def SE3Shortcut(robot, taumax, fmax, vmax, se3traj, Rlist, maxiter, expecteddura
                     #print "*******************************************"
                     print "Success at iteration",it + 1,":", t0, t1,"Deta_t:", t1 - t0 - x.resduration
                     attempt += 1
-                    #print "T:", nnotretimable, "; S:", nnotshorte
+                    #print "T:", nnotretimable, "; S:", nnotshorter , "; C:", ncollision , "; OK:", attempt
+                    #print "************************
