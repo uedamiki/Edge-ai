a2259157430f9b24193a4802c6a02e290340d322
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-04-10 14:16:27 +0000

    remove debug comments

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -316,4 +316,6 @@ def SE3Shortcut(robot, taumax, fmax, vmax, se3traj, Rlist, maxiter, expecteddura
 
             # print "T:", nnotretimable, "; S:", nnotshorter , "; C:", ncollision , "; OK:", attempt
     print "\033[1;32mT:", nnotretimable, "; S:", nnotshorter , "; C:", ncollision , "; OK:", attempt, "\033[0m"
-    print "\033[1;32m", original
+    print "\033[1;32m", originalduration - se3traj.duration ,"sec. shorter\033[0m"
+    t_sc_end = time.time()
+    
