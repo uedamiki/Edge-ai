6ad0a431cdd222b66a79929022d7be1b041c4bf4
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-04-13 16:17:30 +0000

    0.5.19

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -328,4 +328,9 @@ def SE3Shortcut(robot, taumax, fmax, vmax, se3traj, Rlist, maxiter, expecteddura
 
 def ReplaceTransTrajectorySegment(originaltranstraj, transtrajsegment, t0,t1):
     """ReplaceTransTrajectorySegment replaces the segment (t0, t1) in the (arbitrary degree) originaltranstraj 
-    with an (arbitrary degree) transtrajseg
+    with an (arbitrary degree) transtrajsegment.
+    """
+    assert(t1 > t0)
+    
+    newchunkslist = []
+    i0, rem0 = originaltrans
