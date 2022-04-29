7548a3c89308f48870d771580c9bae13555f0fb5
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-04-29 18:32:01 +0000

    chore(package): update eslint-plugin-node to version 8.0.0

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -419,4 +419,7 @@ def Shortcut(robot, taumax, vmax, lietraj,  maxiter, expectedduration = -1,  mea
     
     
     t_sc_start = time.time()
- 
+    originalduration =  lietraj.duration
+    #return shortcuted traj
+    if upperlimit < 0:
+        
