0ba43a889eab98192a1643c98510981bc84c5212
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-05-04 11:27:46 +0000

    cleanup the code

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -453,4 +453,4 @@ def Shortcut(robot, taumax, vmax, lietraj,  maxiter, expectedduration = -1,  mea
             plt.draw()
         if (expectedduration > 0): # check, if newlietraj.duration is short enough, stop SHORTCUTING
             if (lietraj.duration < expectedduration):
-      
+                print "\033[1;32mTrajectory's duration is already shorter than expected time 
