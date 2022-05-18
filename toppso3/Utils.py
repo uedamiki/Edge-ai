d715a970588f6d814f64976aba95c616517a9beb
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-05-18 07:01:50 +0000

    Address reviews

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -519,4 +519,5 @@ def Shortcut(robot, taumax, vmax, lietraj,  maxiter, expectedduration = -1,  mea
                     #print "*******************************************"
                     print "Success at iteration",it + 1,":", t0, t1,"Deta_t:", t1 - t0 - x.resduration
                     attempt += 1
-                    #print "T:", nnotretimable, "; S:", nnotshorter , "; C:",
+                    #print "T:", nnotretimable, "; S:", nnotshorter , "; C:", ncollision , "; OK:", attempt
+                    #print "************************
