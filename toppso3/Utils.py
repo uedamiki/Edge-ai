74ba7fc9f1367e1c2715034452e0845bef9d02fe
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-06-12 14:01:13 +0000

    Updated forms

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -595,4 +595,6 @@ def ReplaceTrajectorySegment(originallietraj, trajsegment, t0, t1):
         r = a.r ## polynomial roots
         for i in range(len(r)):
             r[i] = r[i] - remc1
-        b = np.poly1d(r, True) ## reconstruct a new polynomial 
+        b = np.poly1d(r, True) ## reconstruct a new polynomial from roots
+        ## b is a poly1d object
+        b = b*a.coeffs[0] ## multiply back by a_n ***
