33e86519bac308e071b4c2305c467c34e11639b9
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-06-16 22:28:58 +0000

    Respond to PR comments

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -606,4 +606,5 @@ def ReplaceTrajectorySegment(originallietraj, trajsegment, t0, t1):
 
     ## insert remaining chunk 
     if ic1 < len(originallietraj.trajlist[i1].chunkslist) - 1:
-        for c in originallietraj.trajlist[i1].
+        for c in originallietraj.trajlist[i1].chunkslist[ic1 + 1: len(originallietraj.trajlist[i1].chunkslist)]:
+            newchunksl
