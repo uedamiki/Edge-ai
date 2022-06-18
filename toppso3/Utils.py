b1b7b5d65c5063cc6aaac32d3d501c6afa193354
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-06-18 19:17:02 +0000

    merging git tree

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -607,4 +607,7 @@ def ReplaceTrajectorySegment(originallietraj, trajsegment, t0, t1):
     ## insert remaining chunk 
     if ic1 < len(originallietraj.trajlist[i1].chunkslist) - 1:
         for c in originallietraj.trajlist[i1].chunkslist[ic1 + 1: len(originallietraj.trajlist[i1].chunkslist)]:
-            newchunksl
+            newchunkslist.append(c)
+    ## insert 
+    remtraj1 = Trajectory.PiecewisePolynomialTrajectory(newchunkslist)
+  
