b8bf0e265dcf4656b6617ddc13641148c446cd20
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-08-14 09:34:21 +0000

    Changelog: update

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -757,4 +757,7 @@ def ReadSE3TrajFiles(rlistfilename, se3trajfilename):
     tempse3trajstring = ""
     temp = buff.readline() # ingore the first line which contains "t"
     while buff.pos < buff.len:
-        te
+        temp = buff.readline()
+        if (temp != 't\n'):
+            tempse3trajstring += temp
+     
