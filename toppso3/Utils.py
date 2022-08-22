11d02a1a5e6d75b98852408d29c18dad5e388ea6
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-08-22 19:31:50 +0000

    remove logger

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -767,4 +767,7 @@ def ReadSE3TrajFiles(rlistfilename, se3trajfilename):
     for t_str in se3trajstringlist:
         se3traj = Trajectory.PiecewisePolynomialTrajectory.FromString(t_str)
         se3trajlist.append(se3traj)
-    se3traj = Trajectory.PiecewisePolynomialTrajectory.FromString(TrajS
+    se3traj = Trajectory.PiecewisePolynomialTrajectory.FromString(TrajStringFromTrajList(se3trajlist))
+    return  se3traj, rlist
+
+########
