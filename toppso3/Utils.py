f0dfc814b4cc7b84b5162cb62435c5da7133f4bc
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-09-07 14:53:38 +0000

    chore(package): update eslint to version 3.10.0

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -788,4 +788,6 @@ def PlotSE3(se3traj, rlist,  dt = 0.01, figstart=0,vmax=[],accelmax=[],taumax=[]
     ylabel('Translation velocities (m/s)')
     xlabel('Time (s)')
     for v in vmax[:3]:
-        plt.plot([0, transtraj.duration],[v
+        plt.plot([0, transtraj.duration],[v, v], '-.',color = 'k')
+    for v in vmax[:3]:
+        plt.plot([0, transtraj.duration
