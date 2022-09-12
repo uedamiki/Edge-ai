d20825d55b089b20008d285f159a5bd060c430d8
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-09-12 19:34:30 +0000

    Updated snapshots

--- toppso3/Utils.py
+++ toppso3/Utils.py
@@ -802,4 +802,6 @@ def PlotSE3(se3traj, rlist,  dt = 0.01, figstart=0,vmax=[],accelmax=[],taumax=[]
     ylabel('Forces (N)')
     xlabel('Time (s)')
     for v in fmax[:3]:
-        plt.plot([0, transtraj.duration],[v,
+        plt.plot([0, transtraj.duration],[v, v], '-.',color = 'k')
+    for v in fmax[:3]:
+        plt.plot([0, transtraj.duration],[
