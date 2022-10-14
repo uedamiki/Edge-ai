57e07a7bdf072cc26c14716a8e14f4dea29c52b7
uedamiki <91820992+uedamiki@users.noreply.github.com>
2022-10-14 07:03:07 +0000

    edit pages

--- toppso3/lie.py
+++ toppso3/lie.py
@@ -72,4 +72,7 @@ class LieTraj():
     def Plot(self,dt=0.01,figstart=0,vmax=[],accelmax=[],taumax=[],I=None):
 
         tvect = arange(0, self.duration + dt, dt)
-        omegavect = array([self.EvalOmeg
+        omegavect = array([self.EvalOmega(t) for t in tvect])
+        figure(figstart)
+        clf()
+      
